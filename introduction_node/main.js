c5a40d3942ab67181cfb569d580f0e29d3da0c7b
KHAGESWAR <88511621+khageswar9@users.noreply.github.com>
2022-03-25 12:37:08 +0530

    day one

---
+++ introduction_node/main.js
@@ -0,0 +1,10 @@
+const added = require("./add.js");
+const subtract = require("./subtract.js");
+const multiply = require("./multiply.js");
+const divide = require("./divide.js");
+let a = 10 , b = 5 ;
+
+console.log("addition = ",added(a,b));
+console.log("subtract = ",subtract(a,b));
+console.log("multiply = ",multiply(a,b));
+console.log("divide = ",divide(a,b));
