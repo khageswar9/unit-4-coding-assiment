b8427a9fe2d97b6a3c9f605227a2e2ca10dcf62b
KHAGESWAR <88511621+khageswar9@users.noreply.github.com>
2022-03-25 19:04:21 +0530

    day eleven

---
+++ Authentication/src/controllers/Post.controller.js
@@ -0,0 +1,30 @@
+const express = require("express")
+
+const router = express.Router();
+const authenticate = require("../middlewares/authenticate")
+const Post = require("../models/Post.models");
+
+router.post("", authenticate, async (req, res) => {
+
+    req.body.user_id = req.userID;
+    try{
+        const Post = await Post.create(req.body)
+        return res.status(200).send(Post)
+    }
+    catch(err){
+        return res.status(400).send({message : err.message})
+    }
+ 
+})
+
+router.get("", async (req, res) => {
+    try{
+        const Post = await Post.find()
+        return res.status(200).send(Post)
+    }
+    catch(err){
+        return res.status(400).send({message : err.message})
+    }
+})
+
+module.exports = router;
